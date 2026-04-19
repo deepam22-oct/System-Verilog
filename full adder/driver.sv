3f7f8c8d01ff1a65f82ba4c41bcfa1a18e35e7c5
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-04-19 22:20:23 +0530

    Add driver class for full adder simulation

---
+++ full adder/driver.sv
@@ -0,0 +1,38 @@
+class driver ;
+
+  int no_oftransaction ;
+  virtual intf vif ;
+  mailbox gen2driv ;
+
+  function new(mailbox gen2driv , virtual intf vif);
+    this.gen2driv = gen2driv ;
+    this.vif = vif ;
+  endfunction 
+
+  task reset() ;
+    wait(vif.rst);
+    $display("------------- DRIVER RESET ENDED ------------");
+    vif.a <= 4'b0 ;
+    vif.b <= 4'b0 ;
+    vif.c <= 5'b0 ;
+    wait(vif.rst);
+    $display("------------- DRIVER RESET ENDED ------------");
+  endtask
+  
+  task main();
+    transaction trnas ;
+    forever begin 
+      gen2driv.get(trans);
+      @(posedge vif.clk);
+      vif.valid <=1;
+      vif.a <=trans.a;
+      vif.b <= trans.b;
+      @(posedge vif.clk);
+      vif.valid <=0;
+      trans.c <= vif.c;
+      @(posedge vif.clk);
+      trans.display("Driver");
+      no_transactions++;
+    end
+  endtask
+endclass
