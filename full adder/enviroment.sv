504c7cb71e2356695e1420374c8b7dfc5875c9d7
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-04-20 21:52:17 +0530

    Add environment class for full adder simulation

---
+++ full adder/enviroment.sv
@@ -0,0 +1,54 @@
+`include "transaction.sv"
+`include "generator.sv"
+`include "driver.sv"
+`include "scoreboard.sv"
+`include "monitor.sv"
+
+class enviroment ;
+  generator gen;
+  driver driv;
+  monitor mon;
+  scoreboard scb;
+
+  mailbox gen2driv;
+  mailbox mon2scb;
+
+  function new ( virtual intf vif);
+    this.vif = vif ;
+    gen2driv = new(); 
+    mon2scb = new();
+    gen = new(mailbox gen2driv);
+    driv = new(mailbox gen2driv , virtual intf vif);
+    mon = new(mailbox mon2scb , virtual intf vif);
+    scb = new(mailbox mon2scb);
+  endfunction 
+
+  task pre_test();
+    driv.reset();
+  endtask 
+
+  task test();
+    fork
+    gen.main();
+    driv.main();
+    mon.main();
+    scb.main();
+    join 
+  endtask
+
+ task post_test();
+   wait(gen.ended.triggered);
+   wait(gen.repeat_count == driv.no_transactions);
+   wait(gen.repeat_count == scb.no_transactions);
+ endtask
+      
+task run;
+  pre_test();
+  test();
+  post_test();
+  $finish;
+endtask
+  
+endclass
+
+  
