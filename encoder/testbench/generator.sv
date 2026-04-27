0e6c8db32255b7440e115eb6d1982d5d2380de8a
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-04-27 22:20:49 +0530

    Create generator for encoder testbench

---
+++ encoder/testbench/generator.sv
@@ -0,0 +1,26 @@
+class generator;
+  
+  rand transaction trans;
+  int  repeat_count;
+
+  mailbox gen2driv;
+  event ended;
+  
+ 
+  function new(mailbox gen2driv);
+    this.gen2driv = gen2driv;
+  endfunction
+  
+  
+  task main();
+    repeat(repeat_count) begin
+    trans = new();
+    if( !trans.randomize() ) 
+      $fatal("Gen:: trans randomization failed");
+      trans.display("[ Generator ]");
+      gen2driv.put(trans);
+    end
+    -> ended; 
+  endtask
+  
+endclass
