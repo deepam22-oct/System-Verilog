7bbfb83023d099640da719a34bb58d0820b41c1d
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-05-01 23:10:53 +0530

    Change logic to bit for sel and out signals

--- demux/testbench/interface.sv
+++ demux/testbench/interface.sv
@@ -2,8 +2,8 @@ interface intf(input logic clk , rst ) ;
   
   logic in ;
   logic valid ;
-  logic [2:0]sel ;
+  logic bit [2:0]sel ;
   
-  logic [7:0] out ;
+  logic bit [7:0] out ;
   
 endinterface 
