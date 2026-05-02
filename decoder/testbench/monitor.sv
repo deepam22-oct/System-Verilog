fe69fc875f89cba014d9180260e7ab703f51a11d
Deepam kumar Bisoyi <127032868+deepam22-oct@users.noreply.github.com>
2026-05-02 23:49:35 +0530

    Add monitor class for transaction monitoring

--- decoder/testbench/monitor.sv
+++ decoder/testbench/monitor.sv
@@ -1,1 +1,27 @@
 
+class monitor;
+  
+  virtual intf vif;
+  mailbox mon2scb;
+  
+  function new(virtual intf vif,mailbox mon2scb);
+    this.vif = vif;
+    this.mon2scb = mon2scb;
+  endfunction
+  
+  task main;
+    forever begin
+      transaction trans;
+      trans = new();
+      @(posedge vif.clk);
+      wait(vif.valid);
+      vif.in   <= trans.in;
+      @(posedge vif.clk);
+      trans.out   = vif.out;
+      @(posedge vif.clk);
+      mon2scb.put(trans);
+      trans.display("[ Monitor ]");
+    end
+  endtask
+  
+endclass
